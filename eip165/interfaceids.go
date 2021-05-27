1af0fed018625ad4773fed9afaaa85f51322392e
Chris Hager <chris@linuxuser.at>
2021-05-27 23:12:26 +0200

    eip165 interface ids

---
+++ eip165/interfaceids.go
@@ -0,0 +1,13 @@
+// https://eips.ethereum.org/EIPS/eip-165 interface IDs
+// Used for ERC721 supportsInterface call.
+//
+// https://github.com/metachris/erc721-go-bindings
+package eip165
+
+var (
+	InterfaceIdErc165            = [4]byte{1, 255, 201, 167}  // 0x01ffc9a7
+	InterfaceIdErc721            = [4]byte{128, 172, 88, 205} // 0x80ac58cd
+	InterfaceIdErc721_METADATA   = [4]byte{91, 94, 19, 159}   // 0x5b5e139f
+	InterfaceIdErc721_ENUMERABLE = [4]byte{120, 14, 157, 99}  // 0x780e9d63
+	InterfaceIdErc1155           = [4]byte{217, 182, 122, 38} // 0xd9b67a26
+)
