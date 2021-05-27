d5c0aeff71f642ebc65056fd4b8a5050bc241d5b
Chris Hager <chris@linuxuser.at>
2021-05-27 23:14:16 +0200

    cleanup

--- eip165/interfaceids.go
+++ eip165/interfaceids.go
@@ -5,9 +5,9 @@
 package eip165
 
 var (
-	InterfaceIdErc165            = [4]byte{1, 255, 201, 167}  // 0x01ffc9a7
-	InterfaceIdErc721            = [4]byte{128, 172, 88, 205} // 0x80ac58cd
-	InterfaceIdErc721_METADATA   = [4]byte{91, 94, 19, 159}   // 0x5b5e139f
-	InterfaceIdErc721_ENUMERABLE = [4]byte{120, 14, 157, 99}  // 0x780e9d63
-	InterfaceIdErc1155           = [4]byte{217, 182, 122, 38} // 0xd9b67a26
+	InterfaceIdErc165           = [4]byte{1, 255, 201, 167}  // 0x01ffc9a7
+	InterfaceIdErc721           = [4]byte{128, 172, 88, 205} // 0x80ac58cd
+	InterfaceIdErc721Metadata   = [4]byte{91, 94, 19, 159}   // 0x5b5e139f
+	InterfaceIdErc721Enumerable = [4]byte{120, 14, 157, 99}  // 0x780e9d63
+	InterfaceIdErc1155          = [4]byte{217, 182, 122, 38} // 0xd9b67a26
 )
